ad18a3d4c66b5b9359e9a2c6f593f65e3c99911b
Murat Karabacak <m9urat@gmail.com>
2021-01-28 23:08:28 +0300

    first commit

---
+++ tests/Feature/FirstTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace muratenes\menubuilder\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class FirstTest extends TestCase
+{
+
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
